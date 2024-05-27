db558e5dbee113be1c0fe996909ea4e3576cb794
Lovehansa046 <107402926+Lovehansa046@users.noreply.github.com>
2024-05-27 23:07:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'view/index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'view/about.html'))
+})
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+
+})
