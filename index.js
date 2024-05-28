3a643af0543bc54174376d2e9080f4070d302cdf
GennadiPyton <zikito777@mail.ru>
2024-05-28 09:23:58 +0300

    EX-8

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'view/index.html'))
+    // res.sendFile(path.join(__dirname, 'views', 'view/index.html'))
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'view/about.html'))
+    // res.sendFile(path.join(__dirname, 'views', 'view/about.html'))
+    res.render('about')
 })
 
 
 const PORT = process.env.PORT || 3000
+
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
-
 })
