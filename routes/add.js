58c671622d8aff70d3219dc18ccc4113dab28919
GennadiPyton <zikito777@mail.ru>
2024-05-28 12:51:26 +0300

    EX-32 Исправление косяков

--- routes/add.js
+++ routes/add.js
@@ -3,7 +3,7 @@ const router = Router();
 const Course = require('../models/course');
 
 router.get('/', (req, res) => {
-    res.render('add', {
+    res.render('add',{
         title: 'Добавить курс',
         isAdd: true
     })
