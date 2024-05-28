9d0fbb5ed1c57b39db2c5ec126fd4f40bed38c91
GennadiPyton <zikito777@mail.ru>
2024-05-28 10:03:55 +0300

    EX-17 EX-18

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname, 'views', 'view/index.html'))
     res.render('index')
