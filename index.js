4ba827c3033b999d9426fd170e191f3a84b41cd7
GennadiPyton <zikito777@mail.ru>
2024-05-28 10:22:24 +0300

    EX-22

--- index.js
+++ index.js
@@ -15,32 +15,36 @@ app.set('views', 'views')
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'views', 'view/index.html'))
-    res.render('index', {
-        title: 'Главная страница',
-        isHome: true
-    })
-})
+// app.get('/', (req, res) => {
+//     // res.sendFile(path.join(__dirname, 'views', 'view/index.html'))
+//     res.render('index', {
+//         title: 'Главная страница',
+//         isHome: true
+//     })
+// })
+//
+// app.get('/add', (req, res) => {
+//     // res.sendFile(path.join(__dirname, 'views', 'view/about.html'))
+//     res.render('add', {
+//         title: 'Добавить курс',
+//         isAdd: true
+//     })
+// })
+//
+// app.get('/courses', (req, res) => {
+//     res.render('courses', {
+//         title: 'Курсы',
+//         isCourse: true
+//     })
+// })
 
-app.get('/add', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'views', 'view/about.html'))
-    res.render('add', {
-        title: 'Добавить курс',
-        isAdd: true
-    })
-})
-
-app.get('/courses', (req, res) => {
-    res.render('courses', {
-        title: 'Курсы',
-        isCourse: true
-    })
-})
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', courseesRoutes)
 
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+// const PORT = process.env.PORT || 3000
+//
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`)
+// })
